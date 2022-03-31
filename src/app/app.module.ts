b75a8309d4719a7a7eb3abdbd8f5e910193d16be
car2854 <car2854@gmail.com>
2022-03-31 16:28:51 -0400

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
-import { PagesComponent } from './pages/pages.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PagesComponent
   ],
   imports: [
     BrowserModule,
