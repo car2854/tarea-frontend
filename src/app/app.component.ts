4691fbe6e9ec688fe375558bbe9d6f81855ed077
car2854 <car2854@gmail.com>
2022-03-31 19:21:29 -0400

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,15 @@ import { UserService } from './user.service';
 export class AppComponent {
   title = 'frontend';
   
-  public success: boolean = false;
+  public isSuccess: boolean = false;
+  public isError: boolean = false;
+  public isSubmit: boolean = false;
+
+  public messageError: string = '';
 
   public userForm = this.fb.group({
     name:     ['', [Validators.required]],
-    email:    ['', [Validators.required]],
+    email:    ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required]],
   });
 
@@ -23,23 +27,50 @@ export class AppComponent {
     private userService: UserService
   ){}
 
+  public verifyValidation(campo:string):boolean{
+    
+    if (this.userForm.get(campo)?.invalid){
+      return true
+    }else{
+      return false;
+    }
+
+  }
+
   public createUser = () => {
+    
     if (this.userForm.invalid) return;
 
-    this.userService.createUser(this.userForm.value)
+    this.isSubmit = true;
+
+    setTimeout(() => {
+      this.userService.createUser(this.userForm.value)
       .subscribe({
         error: (err:any) => {
           console.log(err);
+
+          if (err.error.msg) this.messageError = err.error.msg;
+          
+          this.isSubmit = false;
+          this.isError = true;
+          setTimeout(() => {
+            this.isError = false;
+          }, 2000);
         },
         next: (resp:any) => {
-          this.success = true;
+          this.isSuccess = true;
           console.log(resp);
           setTimeout(() => {
-            this.success = false;
+            this.isSuccess = false;
           }, 2000);
+
+          this.isSubmit = false;
+          
           this.userForm.reset();
+
         }
-      })
+      });
+    }, 1500);
       
     
   }
